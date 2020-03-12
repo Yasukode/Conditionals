89a9a94cc45ff009d16e6a878d98ca94e7e00a69
Asante <kwadwo.agyei.2023@student.lhschools.org>
2020-03-12 12:14:45 -0400

    Conditional implemented

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,18 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int numb1 = 12;
+	int numb2 = 100;
+	if(numb1>numb2){
+	    System.out.println(numb1 + " is the bigger number.");
+    }
+    else if (numb1<numb2){
+        System.out.println(numb2 + " is the bigger number.");
+    }
+   else {
+        System.out.println(numb1 + " and "+ numb2+" are equal.");
+    }
+   }
+}
